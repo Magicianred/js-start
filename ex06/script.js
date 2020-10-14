0a8287a2263f7477f9ca0d3f15a22e7a4dd38856
Paulina Hetman <info@pehaa.com>
2020-10-14 09:45:32 +0200

    Update script.js

--- ex06/script.js
+++ ex06/script.js
@@ -1,12 +1,14 @@
 "use strict"
 const question = "De quelle année date ECMA6 (ECMAScript-2015) ?"
 let answer = prompt(question)
-// utilise console.log pour vérifier le type de answer
+/*
+utilise console.log pour vérifier le type de answer
 
-// On peut utiliser la fonction isNaN(answer) ou isFinite(number) pour vérifier si la saisie est numérique
+On peut utiliser la fonction isNaN(answer) ou isFinite(number) pour vérifier si la saisie est numérique
 
-// affiche un de messages suivants selon le cas :
-// Vous n'avez pas saisi de valeur numérique correcte
-// Non, c'était plus tard
-// Non, c'était avant
-// Bravo, c'est une bonne réponse !
+affiche un de messages suivants selon le cas :
+Vous n'avez pas saisi de valeur numérique correcte
+Non, c'était plus tard
+Non, c'était avant
+Bravo, c'est une bonne réponse !
+*/
