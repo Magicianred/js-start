638afe664f662c2f3d58a4071b4ad8eba20281a9
Paulina Hetman <info@pehaa.com>
2020-10-14 09:36:42 +0200

    Update script.js

--- ex04- /script.js
+++ ex04- /script.js
@@ -1,8 +1,11 @@
 "use strict"
-/* demander confirmation 'Confirmez-vous d'être majeur ?' 
-et affecter la réponse à la variable answer */
-/* créer la variable message
-affecter-lui 'Bienvenue !' si la réponse et positive ou 
-'Vous n'être pas autorisé !' dans le cas contraire */
-/* Afficher le message */
-/* 2 solutions avec if et avec l'opérateur conditionnel (ternary operator) */
+/* 
+demander confirmation 'Confirmez-vous d'être majeur ?' 
+affecter la réponse à la variable answer
+créer la variable message
+affecter-lui 'Bienvenue !' si la réponse et positive (answer est true) ou 
+'Vous n'être pas autorisé !' dans le cas contraire
+Afficher le message
+
+préparer deux solutions, une avec if et avec l'autre en utilisant l'opérateur conditionnel (ternary operator) 
+*/
